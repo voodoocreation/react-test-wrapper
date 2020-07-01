42f1165d399b96e5aad1bd0b0aa79e080c3d59ff
Raice Hannay <raice.hannay@xero.com>
2020-07-01 12:19:57 +1200

    add JSDoc comments to all methods

--- src/WrapperWithIntl.tsx
+++ src/WrapperWithIntl.tsx
@@ -3,12 +3,23 @@ import { IntlConfig, IntlProvider } from "react-intl";
 
 import Wrapper from "./Wrapper";
 
+/**
+ * A class to provide a simple interface for setting up your React component unit tests, with support for `react-intl`.
+ *
+ * This class shouldn't be used when using `react-intl-redux`, as that uses a different provider component.
+ */
 export default abstract class WrapperWithIntl<
   C extends React.ComponentType<any>,
   P extends React.ComponentProps<C> = React.ComponentProps<C>
 > extends Wrapper<C, P> {
+  /**
+   * The props to be passed to `react-intl`'s `IntlProvider` component
+   */
   protected abstract intlProviderProps: Partial<IntlConfig>;
 
+  /**
+   * Returns the merged `IntlProvider` props
+   */
   protected get mergedIntlProviderProps() {
     return {
       defaultLocale: "en",
@@ -18,6 +29,9 @@ export default abstract class WrapperWithIntl<
     };
   }
 
+  /**
+   * The component that wraps the component you're testing
+   */
   protected WrappingComponent: React.FC = ({ children }) => (
     <IntlProvider {...this.mergedIntlProviderProps}>{children}</IntlProvider>
   );
--- src/WrapperWithRedux.tsx
+++ src/WrapperWithRedux.tsx
@@ -10,39 +10,93 @@ type TWrapperWithStore<P, S> = ReactWrapper<P> & {
   store: Store<S>;
 };
 
+/**
+ * A class to provide a simple method of setting up your React component tests for apps that use Redux.
+ */
 export default abstract class WrapperWithRedux<
   C extends React.ComponentType<any>,
   S,
   P extends React.ComponentProps<C> = React.ComponentProps<C>
 > extends Wrapper<C, P> {
-  // Properties that remain throughout instance lifecycle
+  // region Properties that remain throughout instance lifecycle
+
+  /**
+   * The default Redux store state for all test scenarios for the current test suite.
+   *
+   * This is set by the `.withDefaultReduxState()` method.
+   */
   protected defaultReduxState: DeepPartial<S> = {};
 
-  // Scenario-specific properties that are cleared whenever shallow/mount/render are called
+  // endregion
+
+  // region Scenario-specific properties that are cleared whenever shallow/mount/render are called
+
+  /**
+   * An array of all actions dispatched during the test scenario lifecycle
+   */
   protected dispatchedActions: AnyAction[] = [];
+
+  /**
+   * The Redux store state specific to the current test scenario.
+   *
+   * This is set by the `.withReduxState()` method and cleared after `.mount()` is called.
+   */
   protected scenarioReduxState: DeepPartial<S> = {};
+
+  /**
+   * The Redux store instance.
+   *
+   * This is set during the `.beforeMount()` hook.
+   */
   protected reduxStore: Store<S> | undefined = undefined;
 
+  // endregion
+
+  /**
+   * Returns an array of all actions that were dispatched during the test scenario lifecycle
+   */
   public get reduxHistory() {
     return this.dispatchedActions;
   }
 
+  /**
+   * Returns the merged Redux store state used by the current test scenario
+   */
   protected get mergedReduxState(): S {
     return merge(this.defaultReduxState, this.scenarioReduxState) as S;
   }
 
+  /**
+   * Sets the default Redux store state used by all test scenarios for the current test suite.
+   *
+   * This method can be chained with other methods.
+   *
+   * @param state The partial Redux store state to set
+   */
   public withDefaultReduxState = (state: DeepPartial<S>) => {
     this.defaultReduxState = state;
 
     return this;
   };
 
+  /**
+   * Sets the Redux store state used by the current test scenario.
+   *
+   * This method can be chained with other methods.
+   *
+   * @param state The partial Redux store state to set
+   */
   public withReduxState = (state: DeepPartial<S>) => {
     this.scenarioReduxState = state;
 
     return this;
   };
 
+  /**
+   * Mounts the component using Enzyme's `mount()` function.
+   *
+   * Returns the `ReactWrapper` instance from `mount()`.
+   */
   // @ts-ignore
   public mount = () => {
     this.dispatchedActions = [];
@@ -64,6 +118,9 @@ export default abstract class WrapperWithRedux<
     return wrapper;
   };
 
+  /**
+   * This method isn't supported when mounting components that read from Redux - use `.mount()` instead
+   */
   public render = () => {
     this.beforeMount();
     this.reset();
@@ -73,6 +130,9 @@ export default abstract class WrapperWithRedux<
     );
   };
 
+  /**
+   * This method isn't supported when mounting components that read from Redux - use `.mount()` instead
+   */
   public shallow = () => {
     this.beforeMount();
     this.reset();
@@ -82,31 +142,56 @@ export default abstract class WrapperWithRedux<
     );
   };
 
+  /**
+   * Resets the Redux action history
+   */
   public resetReduxHistory = () => {
     this.dispatchedActions = [];
   };
 
+  /**
+   * The beforeMount hook.
+   *
+   * This sets the instance of the Redux store used by the current test scenario.
+   */
   protected beforeMount = () => {
     this.reduxStore = this.createStore(this.mergedReduxState, [
       this.reduxHistoryMiddleware,
     ]);
   };
 
+  /**
+   * The middleware for keeping track of actions that were dispatched during the test scenario lifecycle
+   */
   protected reduxHistoryMiddleware: Middleware = () => (next) => (action) => {
     this.dispatchedActions.push(action);
     return next(action);
   };
 
+  /**
+   * The component to wrap the component you're testing - provides the Redux `Provider` component
+   */
   protected WrappingComponent: React.FC = ({ children }) => (
     <Provider store={this.reduxStore!}>{children}</Provider>
   );
 
+  /**
+   * Resets all scenario-specific props, children and Redux store state for the instance
+   */
   protected reset() {
     super.reset();
 
     this.scenarioReduxState = {};
   }
 
+  /**
+   * The method used to create your app's Redux store instance.
+   *
+   * You must implement this method when extending this class.
+   *
+   * @param initialState  The initial Redux store state to create the store instance with
+   * @param middlewares   The middlewares to create the Redux store with
+   */
   protected abstract createStore(
     initialState: DeepPartial<S>,
     middlewares: Middleware[]
